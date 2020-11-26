0b94162f512919d573c22f67d68d9d8599147ff0
chaosannals <chaosannals@outlook.com>
2020-11-26 13:39:16 +0800

    search

--- php/app.php
+++ php/app.php
@@ -15,6 +15,9 @@ $password = 'SecretPassword';
 // 数据管理。
 $ingest  = new Ingest(new Client($host, $port, $timeout));
 $ingest->connect($password);
+echo $ingest->push('messagesCollection', 'defaultBucket', "1231","中文测试搜索。")->getStatus(); // OK
+echo $ingest->push('messagesCollection', 'defaultBucket', "1232","索尼克搜索引擎")->getStatus(); //OK
+echo $ingest->push('messagesCollection', 'defaultBucket', "1233","结果测试")->getStatus(); //OK
 echo $ingest->push('messagesCollection', 'defaultBucket', "1234","hi Shobi how are you?")->getStatus(); // OK
 echo $ingest->push('messagesCollection', 'defaultBucket', "1235","hi are you fine ?")->getStatus(); //OK
 echo $ingest->push('messagesCollection', 'defaultBucket', "1236","Jomit? How are you?")->getStatus(); //OK
@@ -29,4 +32,5 @@ $control->disconnect();
 $search = new Search(new Client($host, $port, $timeout));
 echo $search->connect($password);
 var_dump($search->suggest('messagesCollection', 'defaultBucket', "you"));
+var_dump($search->query('messagesCollection', 'defaultBucket', "索"));
 $search->disconnect();
